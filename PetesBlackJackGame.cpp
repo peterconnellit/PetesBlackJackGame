c60def0abd3380b9bca4259208a1e896c58b73ba
Peter Connell <peterconnellit@gmail.com>
2022-11-29 21:42:18 +0000

    Functions to determine house player turns

--- PetesBlackJackGame.cpp
+++ PetesBlackJackGame.cpp
@@ -282,9 +282,32 @@ public:
     //HousePlayer hitting (hits on 15 or less)
     virtual bool PlayerHit() const;
 
-    //Hides First card
+    //Hides First card at beginning of game and reveals after all players have settled on cards taken
     void HideFirstCard();
 };
+
+HousePlayer::HousePlayer(const string& playerName) :
+    BasePlayer(playerName)
+{}
+
+HousePlayer::~HousePlayer()
+{}
+
+bool HousePlayer::PlayerHit() const
+{
+    return (GetHandTotal() <= 15);
+}
+
+void HousePlayer::HideFirstCard()
+{
+    if (!(m_HandCards.empty())) {
+        m_HandCards[0]->FlipCard();
+    }
+    else {
+        cout << "There is no card to flip!\n";
+    }
+}
+
 
 
 
