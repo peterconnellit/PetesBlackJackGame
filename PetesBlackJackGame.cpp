39fd9fb6cdee2fc27165d2bd641e3a3014001540
Peter Connell <peterconnellit@gmail.com>
2022-11-29 10:15:05 +0000

    Declare Card class enums & << operator overloading

--- PetesBlackJackGame.cpp
+++ PetesBlackJackGame.cpp
@@ -1,7 +1,41 @@
 // PetesBlackJackGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+
+class Card
+{
+public:
+    enum cardRanks {Ace=1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King};
+
+    enum cardSuits {Clubs, Diamonds, Hearts, Spades};
+
+    //We can use overloading to send Card object to standard output operator <<
+    friend ostream& operator << (ostream& os, const Card& aCard);
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main()
 {
